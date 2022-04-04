454d9ba4e230fb9d18bfbf41c2943218cd735c27
Dr.Worm <sky.jackson@icloud.com>
2022-04-03 23:53:00 -0400

    page loads currenta and future weather. still need to add search history and local storage

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ ___
 
 ## Application Details
 - Has a weather dashboard with a form input
-- Search by city and be presented with current and future conditions for that city and that city is added to the search history
-- When you view current weather conditions for that city, then you are presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-- When you view the UV index, then you are presented with a color that indicates whether the conditions are favorable, moderate, or severe
-- When you view future weather conditions for that city, then you are presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+- Search by city and be presented with *current and future* conditions for that city and that city is *added to the search history*
+- When you view current weather conditions for that city, then you are presented with the **city name**, the **date**, an **icon** representation of weather conditions, the **temperature**, the **humidity**, the **wind speed**, and the **UV index**
+- When you view the *UV index*, then you are presented with a **color that indicates whether the conditions** are favorable, moderate, or severe
+- When you view *future weather conditions* for that city, then you are presented with a *5-day forecast* that displays the **date**, an **icon** representation of weather conditions, the **temperature**, the **wind speed**, and the **humidity**
 - When you click on a city in the search history, then you are again presented with current and future conditions for that city
 
 ____
@@ -21,9 +21,8 @@ ____
 <br>
 
 # Challenges Faced
-
-- 
-- 
+- Translating the latitude and longitude of each city typed into the input form into the actual data for the actual city was confusing at first.
+- Making sure all of that data was shown on the page clearly was difficult as well. I had to change my idea for the design a few times throughout making the web app.
 
 <br>
 
@@ -37,4 +36,4 @@ ____
 
 <br>
 
-![Website Screenshot](/assets/images/A)
+![Website Screenshot](/assets/images/Weather-Dash-Screenshot.png)
---
+++ assets/js/script.js
@@ -0,0 +1,238 @@
+var cityInputEl = document.querySelector("#city");
+var searchFormEl = document.querySelector("#search-form");
+var cityContainerEl = document.querySelector("#cards-container");
+
+
+// function to turn the city name into geo locations: latitude and longitude
+var getCityCoordinates = function(city) {
+    // format the Open Weather api Url
+    var geoUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + ",US&limit=1&appid=29db0ef107ead191a2ff9df42aa1bce2"
+    
+    // make a request to the url
+    fetch(geoUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+        response.json().then(function(data) {
+            console.log(data, city);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var state = data[0].state;
+            console.log(lat);
+            console.log(lon);
+            console.log(city);
+            console.log(state)
+            getWeather(lat, lon, city, state);
+        });
+        } else {
+            alert("Error: City Not Found");
+        }
+    })
+    .catch(function(error) {
+        // Notice this '.catch()' getting chained onto the end of the '.then()' method
+        alert("Unable to connect to OpenWeather");
+    });
+};
+
+
+var getWeather = function(lat, lon, city, state) {
+    // format the Open Weather api Url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=29db0ef107ead191a2ff9df42aa1bce2";
+    // make a request to the url
+    fetch(apiUrl)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+        response.json().then(function(data) {
+            console.log(data);
+            console.log(city);
+            currentWeatherCard(data, city, state);
+            futureWeatherCard(data, city, state);
+        });
+    } else {
+        alert("Error: Geocoding.")
+        }
+    })
+    .catch(function(error) {
+        // Notice this '.catch()' getting chained onto the end of the '.then()' method
+        alert("Unable to connect to OpenWeather");
+    });
+};
+
+
+var currentWeatherCard = function(data, city, state) {
+    // create city card
+    var cityEl = document.createElement("div");
+    cityEl.classList = "card col-5 col-md-3 col-lg-2 shadow border border-dark card-group";
+    
+    // get image icon from data and append to card
+    var imgEl = document.createElement("img");
+    var icon = data.current.weather[0].icon;
+    imgEl.classList = "card-img-top";
+    imgEl.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    cityEl.appendChild(imgEl);
+
+    var cardBodyEl = document.createElement("div");
+    cardBodyEl.classList = "card-body";
+
+    cityEl.appendChild(cardBodyEl);
+
+    var cardTitleEl = document.createElement("h5");
+    cardTitleEl.classList = "card-title";
+    cardTitleEl.textContent = city + ", " + state;
+
+    var dateEl = document.createElement("p");
+    dateEl.classList = "card-text";
+    var unix = data.current.dt;
+    var momentDate = moment.unix(unix).format("dddd, MMMM Do, LT");
+    dateEl.textContent = momentDate;
+
+    cardBodyEl.appendChild(cardTitleEl);
+    cardBodyEl.appendChild(dateEl);
+
+    createCardList(data, cityEl);
+
+   
+    // append the card to the main body
+    cityContainerEl.appendChild(cityEl);
+
+};
+
+
+var createCardList = function(data, cityEl) {
+    var listGroup = document.createElement("ul");
+    listGroup.classList = "list-group list-group-flush";
+
+    var tempEL = document.createElement("li");
+    tempEL.classList = "list-group-item";
+    tempEL.innerText = "Temp: " + data.current.temp + "°F";
+    listGroup.appendChild(tempEL);
+
+    var humidityEl = document.createElement("li");
+    humidityEl.classList = "list-group-item";
+    humidityEl.innerText = "Humidity: " + data.current.humidity + "%";
+    listGroup.appendChild(humidityEl);
+
+    var windEl = document.createElement("li");
+    windEl.classList = "list-group-item";
+    windEl.innerText = "Wind Speed: " + data.current.wind_speed + "mph";
+    listGroup.appendChild(windEl);
+
+    var uvEl = document.createElement("li");
+    var UV = data.current.uvi
+    uvEl.innerText = "UV Index: " + UV;
+    // change UV Index color due to intensity
+    if (UV <= 4) {
+        uvEl.classList = "uv-light list-group-item";
+    } else if (UV > 4 && UV < 8) {
+        uvEl.classList = "uv-moderate list-group-item";
+    } else {
+        uvEl.classList = "uv-severe list-group-item";
+    };
+
+    listGroup.appendChild(uvEl);
+
+    cityEl.appendChild(listGroup);
+};
+        
+        
+var futureWeatherCard = function (data, city, state) {
+    // loop through array of future dates to get data
+    for (var i = 1; i < 6; i++) {
+        console.log(data.daily[i].dt);
+    // create future city card
+    var futureCard = document.createElement("div");
+    futureCard.classList = "card col-5 col-md-3 col-lg-2 shadow border border-dark card-group";
+    
+    // get image icon from data and append to card
+    var imgEl = document.createElement("img");
+    var icon = data.daily[i].weather[0].icon;
+    imgEl.classList = "card-img-top";
+    imgEl.src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+    futureCard.appendChild(imgEl);
+
+    var cardBodyEl = document.createElement("div");
+    cardBodyEl.classList = "card-body";
+
+    futureCard.appendChild(cardBodyEl);
+
+    var cardTitleEl = document.createElement("h5");
+    cardTitleEl.classList = "card-title";
+    cardTitleEl.textContent = city + ", " + state;
+
+    var dateEl = document.createElement("p");
+    dateEl.classList = "card-text";
+    var unix = data.daily[i].dt;
+    var momentDate = moment.unix(unix).format("dddd, MMMM Do, LT");
+    dateEl.textContent = momentDate;
+
+    cardBodyEl.appendChild(cardTitleEl);
+    cardBodyEl.appendChild(dateEl);
+
+    futureCardList(data, futureCard, i);
+
+   
+    // append the card to the main body
+    cityContainerEl.appendChild(futureCard);
+    };
+};
+
+
+var futureCardList = function(data, futureCard, i) {
+    var listGroup = document.createElement("ul");
+    listGroup.classList = "list-group list-group-flush";
+
+    var tempEL = document.createElement("li");
+    tempEL.classList = "list-group-item";
+    tempEL.innerText = "High: " + data.daily[i].temp.max + "°F | Low: " + data.daily[i].temp.min;
+    listGroup.appendChild(tempEL);
+
+    var humidityEl = document.createElement("li");
+    humidityEl.classList = "list-group-item";
+    humidityEl.innerText = "Humidity: " + data.daily[i].humidity + "%";
+    listGroup.appendChild(humidityEl);
+
+    var windEl = document.createElement("li");
+    windEl.classList = "list-group-item";
+    windEl.innerText = "Wind Speed: " + data.daily[i].wind_speed + "mph";
+    listGroup.appendChild(windEl);
+
+    var uvEl = document.createElement("li");
+    var UV = data.daily[i].uvi
+    uvEl.innerText = "UV Index: " + UV;
+    // change UV Index color due to intensity
+    if (UV <= 4) {
+        uvEl.classList = "uv-light list-group-item";
+    } else if (UV > 4 && UV < 8) {
+        uvEl.classList = "uv-moderate list-group-item";
+    } else {
+        uvEl.classList = "uv-severe list-group-item";
+    };
+
+    listGroup.appendChild(uvEl);
+
+    futureCard.appendChild(listGroup);
+};
+
+
+// function the takes user inputed data and checks it with APIs
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // console.log(event.target)
+
+    // get name of user inputed city
+    var city = cityInputEl.value.trim();
+    // console.log(city);
+
+    if (city) {
+        getCityCoordinates(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter an existing city.");
+    }
+};
+
+
+searchFormEl.addEventListener("submit", formSubmitHandler);
